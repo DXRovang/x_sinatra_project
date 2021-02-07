de986ba02cebcf8dc54f55011b18f119850e86e8
Alexander Rovang <wolf_vii@yahoo.com>
2021-02-07 11:14:18 -0500

    added empty CRUD routes to artworks_controller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,3 +1,33 @@
 class ArtworksController < ApplicationController
-  
+  get 'artworks' do #index
+    
+  end
+
+  get 'artworks/new' do #new form
+    
+  end
+
+  post 'artworks' do #create
+    
+  end
+
+  get 'artworks/:id' do #show
+    
+  end
+
+  get 'artworks/:id/edit' do #edit form
+    
+  end
+
+  post 'artworks/:id' do #update
+    
+  end
+
+  get 'artworks/:id/delete' do #delete confirmation
+    
+  end
+
+  post 'artworks/:id/delete' do #delete
+    
+  end
 end
