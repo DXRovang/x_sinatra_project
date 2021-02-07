3c15e179f935efc31b7b31c4437fafcb748486d7
Alexander Rovang <wolf_vii@yahoo.com>
2021-02-07 11:11:49 -0500

    added empty CRUD routes to artists_controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,3 +1,35 @@
 class ArtistsController < ApplicationController
+
+  get 'artists' do #index
+    
+  end
+
+  get 'artists/new' do #new form
+    
+  end
+
+  post 'artists' do #create
+    
+  end
+
+  get 'artists/:id' do #show
+    
+  end
+
+  get 'artists/:id/edit' do #edit form
+    
+  end
+
+  post 'artists/:id' do #update
+    
+  end
+
+  get 'artists/:id/delete' do #delete confirmation
+    
+  end
+
+  post 'artists/:id/delete' do #delete
+    
+  end
   
 end
