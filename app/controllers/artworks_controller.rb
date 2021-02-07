9d59a2fd404e353691347957a29da5ff44b80274
Alexander Rovang <wolf_vii@yahoo.com>
2021-02-07 12:24:34 -0500

    filled in logic for new artwork in artworks_controller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -6,11 +6,31 @@ class ArtworksController < ApplicationController
   end
 
   get '/artworks/new' do #new form
-    
+    if !logged_in?
+      redirect '/login'
+    else
+      erb :'artworks/new'
+    end
   end
 
   post '/artworks' do #create
     
+    if Artist.find_by(name: params[:artist_id]) != nil
+      artist = Artist.find_by(name: params[:artist_id])
+      artwork = Artwork.new(
+        name: params[:name], 
+        medium: params[:medium], 
+        genre: params[:genre], 
+        price: params[:price], 
+        date: params[:date], 
+        artist_id: artist.id,
+        user_id: current_user.id 
+      )
+      artwork.save
+      redirect "/artworks"
+    else
+      redirect "/artworks/new"
+    end
   end
 
   get '/artworks/:id' do #show
