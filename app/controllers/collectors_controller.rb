30e9801f7c72f7d4eae8903047fae0d7a66ab673
Alexander Rovang <wolf_vii@yahoo.com>
2021-02-07 11:17:04 -0500

    added empty CRUD routes to collectors_controller

--- app/controllers/collectors_controller.rb
+++ app/controllers/collectors_controller.rb
@@ -16,12 +16,32 @@ class CollectorsController < ApplicationController
     end
   end
 
-  get "/index" do
+  get "/index" do #index
     if !logged_in? 
       redirect '/login'
     else
       erb :'collectors/index'
     end
+  end
+
+  get 'collectors/:id' do #show
+    
+  end
+
+  get 'collectors/:id/edit' do #edit form
+    
+  end
+
+  post 'collectors/:id' do #update
+    
+  end
+
+  get 'collectors/:id/delete' do #delete confirmation
+    
+  end
+
+  post 'collectors/:id/delete' do #delete
+    
   end
 
 end
